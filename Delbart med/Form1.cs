b1e361068937b73a88bca2b1c4f79155b7a7f417
lule0329 <lule0329@S26507>
2024-02-01 13:59:07 +0100

    Add project files.

---
+++ Delbart med/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Delbart_med
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int tal;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+
+            if (int.TryParse(text, out int textNummer))
+            {
+                // Är det delbart med 15?
+                if (textNummer % 15 == 0)
+                {
+                    checkBox1.Checked = true;
+                }
+                else
+                {
+                    checkBox1.Checked = false;
+                }
+
+                // Är det delbart med 5?
+                if (textNummer % 5 == 0)
+                {
+                    checkBox2.Checked = true;
+                }
+                else
+                {
+                    checkBox2.Checked = false;
+                }
+
+                // Är det delbart med 3?
+                if (textNummer % 3 == 0)
+                {
+                    checkBox3.Checked = true;
+                }
+                else
+                {
+                    checkBox3.Checked = false;
+                }
+            }
+        }
+    }
+}
